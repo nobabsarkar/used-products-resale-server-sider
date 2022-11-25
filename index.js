1b45f08aab120e6f24ac0c889a3b865a40ef5521
Nobab <nobabsarkar2016@gmail.com>
2022-11-25 11:18:36 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+// useProduct
+// knF5zC3H2ci7zLOl;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@amarclastar.oxggard.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const useCollection = client.db("useProducts").collection("product");
+
+    app.get("/bikeCollection", async (req, res) => {
+      const query = {};
+      const options = await useCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+run().catch(console.log());
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
+});
